7eb5e7c50c64b548c7cc526d70d342acb92af730
h-varmazyar <hossein.varmazyar@yahoo.com>
2024-05-30 10:05:58 +0330

    feat: add dsn config for gorm pack

--- go.mod
+++ go.mod
@@ -6,15 +6,26 @@ require (
 	github.com/caarlos0/env/v6 v6.9.2
 	github.com/go-playground/validator/v10 v10.11.0
 	github.com/golang-jwt/jwt v3.2.2+incompatible
+	github.com/google/uuid v1.6.0
 	github.com/gorilla/mux v1.8.0
 	github.com/joho/godotenv v1.4.0
+	github.com/sirupsen/logrus v1.9.3
+	gorm.io/driver/mysql v1.5.6
+	gorm.io/driver/postgres v1.5.7
+	gorm.io/gorm v1.25.10
 )
 
 require (
 	github.com/go-playground/locales v0.14.0 // indirect
 	github.com/go-playground/universal-translator v0.18.0 // indirect
+	github.com/go-sql-driver/mysql v1.7.0 // indirect
+	github.com/jackc/pgpassfile v1.0.0 // indirect
+	github.com/jackc/pgservicefile v0.0.0-20221227161230-091c0ba34f0a // indirect
+	github.com/jackc/pgx/v5 v5.4.3 // indirect
+	github.com/jinzhu/inflection v1.0.0 // indirect
+	github.com/jinzhu/now v1.1.5 // indirect
 	github.com/leodido/go-urn v1.2.1 // indirect
-	golang.org/x/crypto v0.0.0-20211215153901-e495a2d5b3d3 // indirect
-	golang.org/x/sys v0.0.0-20210806184541-e5e7981a1069 // indirect
-	golang.org/x/text v0.3.7 // indirect
+	golang.org/x/crypto v0.14.0 // indirect
+	golang.org/x/sys v0.13.0 // indirect
+	golang.org/x/text v0.13.0 // indirect
 )
--- gorm/configs.go
+++ gorm/configs.go
@@ -1,36 +1,50 @@
 package gormext
 
-import "fmt"
+import (
+	"fmt"
+	"regexp"
+)
+
+const dsnRegex = `^(?P<scheme>[^:]*):host=(?P<host>[^;]*);port=(?P<port>[^;]*);dbname=(?P<database>[^;]*)\?sslmode=(?P<ssl>[^:]*)?$`
 
 type Configs struct {
-	DbType      Type   `mapstructure:"type"`
-	Port        uint16 `mapstructure:"port"`
-	Host        string `mapstructure:"host"`
-	Username    string `mapstructure:"username"`
-	Password    string `mapstructure:"password"`
-	Name        string `mapstructure:"name"`
-	IsSSLEnable bool   `mapstructure:"is_ssl_enable"`
+	DbType      Type   `env:"TYPE" mapstructure:"type"`
+	Port        uint16 `env:"PORT" mapstructure:"port"`
+	Host        string `env:"HOST" mapstructure:"host"`
+	Username    string `env:"UESRNAME" mapstructure:"username"`
+	Password    string `env:"PASSWORD" mapstructure:"password"`
+	Name        string `env:"NAME" mapstructure:"name"`
+	IsSSLEnable bool   `env:"IS_SSL_ENABLE" mapstructure:"is_ssl_enable"`
+	DSN         string `env:"DSN,file"`
 }
 
 func (c *Configs) generateDSN() string {
-	dsn := ""
+	if c.DSN != "" {
+		return c.DSN
+	}
 	switch c.DbType {
 	case PostgreSQL:
-		dsn = fmt.Sprintf("postgresql://%v:%v@%v:%v/%v?sslmode=%v", c.Username, c.Password, c.Host, "%v", c.Name, "%v")
+		c.DSN = fmt.Sprintf("postgresql://%v:%v@%v:%v/%v?sslmode=%v", c.Username, c.Password, c.Host, "%v", c.Name, "%v")
 		if c.Port != 0 {
-			dsn = fmt.Sprintf(dsn, c.Port)
+			c.DSN = fmt.Sprintf(c.DSN, c.Port)
 		}
 		if c.IsSSLEnable {
-			dsn = fmt.Sprintf(dsn, "Enable")
+			c.DSN = fmt.Sprintf(c.DSN, "Enable")
 		} else {
-			dsn = fmt.Sprintf(dsn, "disable")
+			c.DSN = fmt.Sprintf(c.DSN, "disable")
 		}
 	}
-	return dsn
+	return c.DSN
 }
 
 func (c *Configs) rootDSN() string {
 	dsn := ""
+	if c.DSN != "" {
+		r, err := regexp.Compile(dsnRegex)
+		r.FindAll()
+		dsn = c.DSN
+	}
+	//^(?P<scheme>[^:]*):host=(?P<host>[^;]*);port=(?P<port>[^;]*);dbname=(?P<database>[^;]*)(\?sslmode=[^:]*){0,1}$
 	switch c.DbType {
 	case PostgreSQL:
 		dsn = fmt.Sprintf("postgresql://%v:%v@%v:%v?sslmode=%v", c.Username, c.Password, c.Host, "%v", "%v")
